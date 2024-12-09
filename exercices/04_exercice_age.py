962b9f9e15e1341c38b91aa8a24b11997372ed51
[ArkunLeSerein] <[parsy.julien@hotmail.fr]>
2024-12-09 15:14:37 +0100

    ajout exercice 4

---
+++ exercices/04_exercice_age.py
@@ -0,0 +1,10 @@
+# crée la variable qui contient l'âge légal
+age_legal = 18 
+# Crée la variable qui contient l'âge de la personne
+age = (int(input("Saisissez votre âge : ")))
+
+# On crée la variable qui contient le resultat
+result = age >= age_legal 
+
+# On affiche le resultat
+print(result)
