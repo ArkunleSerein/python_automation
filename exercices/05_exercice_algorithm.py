090d7e256b22e9592ddb3ee0de4e23b1976e5cf9
[ArkunLeSerein] <[parsy.julien@hotmail.fr]>
2024-12-09 16:09:56 +0100

    ajout de l'exercice 5

---
+++ exercices/05_exercice_algorithm.py
@@ -0,0 +1,12 @@
+# On crée la variable "saisi" avec un input afin de saisir le nombre voulu.
+saisi = int(input("Veuillez saisir un nombre entier : "))
+# On crée ensuite la variable résutat qui va contenir la formule demandée
+resultat = saisi % 3 == 0
+
+
+# on crée un algorithme qui renvoi la phrase "Le nombre est divisible par 3" si le modulo == 0 
+# sinon il renvoit "Le nombre n'est pas divisible par 3".
+if resultat:
+    print("Le nombre est divisible par 3")
+else:
+    print("Le nombre n'est pas divisible par 3")
