e70af34e960c2ce303ed62ea8fe09ea61f63fd7a
[ArkunLeSerein] <[parsy.julien@hotmail.fr]>
2024-12-09 14:37:08 +0100

    MàJ

--- exercices/03_exercice_math.py
+++ exercices/03_exercice_math.py
@@ -8,6 +8,8 @@ hauteur = float(input("Saisir le rayon du cône : "))
 # On crée la variable volume qui va contenir le calcul du volume d'un cône droit
 # ajout de la fonction round(,2) qui arondi le résultat 2 chiffres après la virgule
 volume = round((pi * pow(rayon, 2)) * (hauteur / 3), 2)
+
+# On crée une variable qui contient le résultat du calcul, contenu dans la variable volume
 resultat = f"Le volume du cône est égal à {volume}"
 
 # On appelle la variable volume sour forme d'Integer
