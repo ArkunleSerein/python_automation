f3fa627e158b51f8348ab090959269719ef63e5e
[ArkunLeSerein] <[parsy.julien@hotmail.fr]>
2024-12-09 14:28:11 +0100

    ajout de l'exercice 3

---
+++ demos/03_operateurs.py
@@ -0,0 +1,21 @@
+# Import du module math 
+from math import *
+
+# Pour utiliser une variable avec (import math) il faut l'appeler dans la variable ex : pi = math.pi
+
+nb = pi
+print(nb)
+# __Les opérateurs__
+
+a = 10 + 15 # Addition
+b = 10 % 15 # Modulo (reste de la division)
+c = 10 * 15 # Multiplication
+d = 10 ** 15 # Puissance de
+e = 15 - 10 # Soustraction
+f = 10 / 2 # Division 
+g = 10 // 3 # division entière sans virgule
+
+print(a, b, c, d, e, f, g)
+
+b -= 5
+print(b)
---
+++ exercices/03_exercice_math.py
@@ -0,0 +1,14 @@
+# On importe la bibliothèque math
+from math import *
+
+# On crée les variables rayon et hauteur qui vont contenir les nombres
+rayon = float(input("Saisir la hauteur du cône : "))
+hauteur = float(input("Saisir le rayon du cône : "))
+
+# On crée la variable volume qui va contenir le calcul du volume d'un cône droit
+# ajout de la fonction round(,2) qui arondi le résultat 2 chiffres après la virgule
+volume = round((pi * pow(rayon, 2)) * (hauteur / 3), 2)
+resultat = f"Le volume du cône est égal à {volume}"
+
+# On appelle la variable volume sour forme d'Integer
+print(resultat)
