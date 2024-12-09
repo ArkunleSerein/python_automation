0c8a126fb5befab7bdb54c09f7491d47954996b4
[ArkunLeSerein] <[parsy.julien@hotmail.fr]>
2024-12-09 11:11:20 +0100

    Ajout des dossiers demos et exercices ainsi que les fichiers 01_variables, 01_exercice

---
+++ demos/01_variables.py
@@ -0,0 +1,20 @@
+# __Démos python__
+
+# Affiche Hello world dans la console.
+print("Hello world")
+
+# Les variables et les types (on peut indiqué les types en utilsant(: letype après la variable), ce n'est pas obligatoire)
+mon_int : int = 514 # variable de type Integer.
+mon_float : float = 3.14 # variable de type Float.
+ton_bool : bool = True # variable de type Boolean.
+ton_string : str = "Le python c'est cool" # variable de type String.
+mon_none = None # variable qui n'affiche rien
+
+# Crée un calcul des variables mon_float + mon_int + 1 dans la variable mon calcul.
+mon_calcul = mon_float + 1 + mon_int
+# Utilise la variable mon_calcul
+print(mon_calcul)
+# Affiche le type de la variable mon_calcul
+print(type(mon_calcul))
+# Affiche une phrase avant la variable mon_calcul.
+print("Mon calcul est égal à", mon_calcul)
---
+++ exercices/01_exercice.py
@@ -0,0 +1,11 @@
+# Exercice, la variable 'a' doit être égal à la variable 'b'
+a = 1 
+b = 2
+
+# Création de variable temporaire nommée 'tmp' pour changer la valeur d'une autre variable
+tmp = a
+a = b
+b = tmp 
+
+# Affiche la variable 'a' après la chaîne de caractère (string "a =")
+print("a =",a)
